ef02330ee46d4c55ad361fa9950fc385d307a715
yuqirong <985856962@qq.com>
2018-03-21 21:07:13 +0800

    g

---
+++ 2018-3-21/梁琰沁的分享（redux原理）.js
@@ -0,0 +1,171 @@
+// Library code
+function createStore (reducer) {
+
+  //1.state
+  //2.getState
+  //3.subscribe
+  //4.dispatch
+
+  let state
+  let listeners = []
+
+  const getState = () => state
+
+  const subscribe = (listener) => {
+    listeners.push(listener)
+    return () => {
+      listeners = listeners.filter((l) => l !== listener)
+    }
+  }
+
+  const dispatch = (action) => {
+    state = reducer(state, action)
+    listeners.forEach((listener) => listener())
+  }
+
+  return {
+    getState,
+    subscribe,
+    dispatch,
+  }
+}
+
+const ADD_TODO = 'ADD_TODO'
+const REMOVE_TODO = 'REMOVE_TODO'
+const TOGGLE_TODO = 'TOGGLE_TODO'
+const ADD_GOAL = 'ADD_GOAL'
+const REMOVE_GOAL = 'REMOVE_GOAL'
+
+function addTodoAction (todo) {
+  return {
+    type: ADD_TODO,
+    todo,
+  }
+}
+
+function removeTodoAction (id) {
+  return {
+    type: REMOVE_TODO,
+    id,
+  }
+}
+
+function toggleTodoAction (id) {
+  return {
+    type: TOGGLE_TODO,
+    id,
+  }
+}
+
+function addGoalAction (goal) {
+  return {
+    type: ADD_GOAL,
+    goal,
+  }
+}
+
+function removeGoalAction (id) {
+  return {
+    type: REMOVE_GOAL,
+    id,
+  }
+}
+
+
+//reducer
+function todos (state = [], action) {
+  switch(action.type) {
+    case ADD_TODO :
+      return state.concat([action.todo])
+    case REMOVE_TODO :
+      return state.filter((todo) => todo.id !== action.id)
+    case TOGGLE_TODO :
+      return state.map((todo) => todo.id !== action.id ? todo :
+        Object.assign({}, todo, {complete: !todo.complete})
+      )
+    default :
+      return state
+  }
+}
+
+function goals (state = [], action) {
+  switch(action.type) {
+    case ADD_GOAL :
+      return state.concat([action.goal])
+    case REMOVE_GOAL :
+      return state.filter((goal) => goal.id !== action.id)
+    default :
+      return state
+  }
+}
+//combine reducers:
+function app (state = {}, action) {
+  return {
+    todos: todos(state.todos, action),
+    goals: goals(state.goals, action)
+  }
+}
+
+const store = createStore(app)
+
+store.subscribe(() => {
+  console.log('The new state is: ', store.getState())
+})
+
+store.dispatch(addTodoAction({
+  id: 0,
+  name: 'Walk the dog',
+  complete: false,
+}))
+
+store.dispatch(addTodoAction({
+  id: 1,
+  name: 'Wash the car',
+  complete: false,
+}))
+
+store.dispatch(addTodoAction({
+  id: 2,
+  name: 'Go to the gym',
+  complete: true,
+}))
+
+store.dispatch(removeTodoAction(1))
+
+store.dispatch(toggleTodoAction(0))
+
+
+
+
+
+
+
+// const Context = React.createContext()
+// Context.Provider
+// Context.Comsumer
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <ConnectedApp />
+//   </Provider>,
+//   document.getElementById('app')
+// )
+
+// class ConnectedApp extends React.Component {
+//   render() {
+//     return (
+//       <Context.Consumer>
+//         {(store) => (
+//           <App store={store} />
+//         )}
+//       </Context.Consumer>
+//     )
+//   }
+// }
+
+// class App extends React.Component{
+
+// }
+// const ConnectedApp = ReactRedux((state) => ({
+//   loading: state.loading
+// }))(App)
